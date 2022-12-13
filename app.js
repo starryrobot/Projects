887a56c8d79e4dcfbdb93d5401c9e997cc0f2037
starryrobot <g_d@tuta.io>
2022-12-13 01:00:32 +0000

    Code changes

--- app.js
+++ app.js
@@ -4,25 +4,93 @@ let form = document.querySelector("form");
 const dateText = document.getElementById("date");
 const fromLocal = JSON.parse(localStorage.getItem("Entry"));
 const statusLocal = document.getElementById("status-local");
+const statusClear = document.getElementById("status-clear");
 const callbackText = document.getElementById("callbacks");
+const formInteract = document.querySelectorAll(".form-interact");
+const interactText = document.getElementById("interact-text");
 let formArray = [];
 let ulHeight = 0;
 let callbackCounter = 0;
 let formEnabled = false;
+let interactState = false;
+let clearState = false;
 
 /* Add DOMContentLoaded to load from Local Storage */
 
-window.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("DOMContentLoaded", onLoad);
+
+function onLoad() {
   if (fromLocal) {
     console.log(fromLocal);
     formArray = fromLocal;
-    statusLocal.innerHTML = `<i class="fa-solid fa-hard-drive" title="Local Storage found"></i>`;
+    statusLocal.innerHTML = `Loaded from Local Storage <i class="fa-solid fa-check"></i>`;
     callbackText.textContent = formArray.length;
     renderForm(fromLocal);
   } else {
     console.log("Not found");
+    statusLocal.innerHTML = `Loaded from Local Storage <i class="fa-solid fa-xmark"></i></i>`;
+  }
+}
+
+/* Clear LocalStorage */
+statusClear.addEventListener("click", function () {
+  let alert = confirm(
+    "Continuing will delete all callbacks and refresh the page!\r\n\r\nThis might not be able to be undone!\r\n\r\nContinue?"
+  );
+  if (alert) {
+    localStorage.removeItem("Entry");
+    location.reload();
+    onLoad();
   }
 });
+
+/* Form interaction */
+
+formInteract.forEach(function (element) {
+  const faChevronDown = `<i class="fa-solid fa-chevron-down fa-2xl"></i>`;
+  const faChevronUp = `<i class="fa-solid fa-chevron-up fa-2xl"></i>`;
+  const showForm = `<span class="normal" id="interact-text">Show Form</span>`;
+  const hideForm = `<span class="normal" id="interact-text">Hide Form</span>`;
+  element.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("interact-form")) {
+      form.classList.toggle("hidden");
+      if (form.classList.contains("hidden")) {
+        console.log(this.innerHTML);
+        this.innerHTML = faChevronUp + showForm;
+        setTimeout(function () {
+          form.style.display = "none";
+        }, 1000);
+      } else {
+        console.log(this.innerHTML);
+        this.innerHTML = faChevronDown + hideForm;
+        setTimeout(function () {
+          form.style.display = "initial";
+        }, 1000);
+      }
+    } else if (styles.contains("interact-stats")) {
+    }
+  });
+});
+
+// formInteract.addEventListener("click", function (element) {
+//   const faChevronDown = `<i class="fa-solid fa-chevron-down fa-2xl"></i>`;
+//   const faChevronUp = `<i class="fa-solid fa-chevron-up fa-2xl"></i>`;
+//   const showForm = `<span class="normal" id="interact-text">Show Form</span>`;
+//   const hideForm = `<span class="normal" id="interact-text">Hide Form</span>`;
+//   form.classList.toggle("hidden");
+//   if (form.classList.contains("hidden")) {
+//     formInteract.innerHTML = faChevronUp + showForm;
+//     setTimeout(function () {
+//       form.style.display = "none";
+//     }, 1000);
+//   } else {
+//     formInteract.innerHTML = faChevronDown + hideForm;
+//     setTimeout(function () {
+//       form.style.display = "initial";
+//     }, 1000);
+//   }
+// });
 
 /* Add EventListener to form */
 
@@ -79,7 +147,7 @@ function renderForm(form) {
   }
   ulEl.innerHTML = listItems;
   /* Scroll to bottom of log-area element */
-  logArea.scrollTop = logArea.scrollHeight;
+  // logArea.scrollTop = logArea.scrollHeight;
 }
 
 /* Time function */
